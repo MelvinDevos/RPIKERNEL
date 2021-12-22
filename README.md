dda1e2e065f3388ce2a8e079c323e0359240224b
Melvin Devos <melvin.devos@student.vives.be>
2021-12-22 23:54:02 +0000

    Update (GPIO not working)

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Kernel module that controls GPIO
 
 sudo rmmod main
 make
-sudo insmod main.ko gpio_array=5,12 level_array=1,0 toggle_array=0,5
+sudo insmod main.ko gpio_array=17,27 level_array=1,0 toggle_array=8,5
 dmesg
--- main.c
+++ main.c
@@ -6,15 +6,59 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/stat.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Melvin Devos");
 
-static int gpio_array[20];
-static int level_array[20];
-static int toggle_array[20];
+/* Task handle to identify thread */
+static struct task_struct *toggle1 = NULL;
+static struct task_struct *toggle2 = NULL;
+
+static int gpio_array[2];
+static int level_array[2];
+static int toggle_array[2];
 
 static int arr_argc = 0;
+
+static struct gpio gpio_output[] = {
+    {17, GPIOF_OUT_INIT_LOW, "LED 1"},
+    {27, GPIOF_OUT_INIT_LOW, "LED 2"},
+};
+
+static int toggle_thread1(void *data)
+{
+    printk(KERN_INFO "%s\n", __func__);
+    printk(KERN_INFO "toggle thread speed = %d\n", toggle_array[0] * 100);
+
+    // loop until killed ...
+    while (!kthread_should_stop())
+    {
+        gpio_set_value(gpio_output[0].gpio, !gpio_get_value(gpio_output[0].gpio));
+
+        mdelay(toggle_array[0] * 100);
+    }
+
+    return 0;
+}
+
+static int toggle_thread2(void *data)
+{
+    printk(KERN_INFO "%s\n", __func__);
+    printk(KERN_INFO "toggle thread speed = %d\n", toggle_array[0] * 100);
+
+    // loop until killed ...
+    while (!kthread_should_stop())
+    {
+        gpio_set_value(gpio_output[1].gpio, !gpio_get_value(gpio_output[1].gpio));
+
+        mdelay(toggle_array[1] * 100);
+    }
+
+    return 0;
+}
 
 /*
  * module_param_array(name, type, num, perm);
@@ -34,22 +78,59 @@ MODULE_PARM_DESC(level_array, "An array of integers");
 module_param_array(toggle_array, int, &arr_argc, 0000);
 MODULE_PARM_DESC(toggle_array, "An array of integers");
 
-static int __init hello_5_init(void)
+static int __init gpio_blink_init(void)
 {
     int i;
 
+    int speed1 = toggle_array[1] * 100;
+    int speed2 = toggle_array[2] * 100;
+
+    printk(KERN_INFO " %d argumenten.\n", arr_argc);
     for (i = 0; i < arr_argc; i++)
     {
-        printk(KERN_INFO "myintArray[%d] = %d\n", i, gpio_array[i]);
+
+        gpio_output[i].gpio = gpio_array[i];
+        gpio_set_value(gpio_output[i].gpio, level_array[i]);
+        printk(KERN_INFO "GPIO%d: %d met snelheid %d.\n", gpio_output[i].gpio, level_array[i], toggle_array[i]);
     }
-    printk(KERN_INFO "got %d arguments for myintArray.\n", arr_argc);
+
+    toggle1 = kthread_create(toggle_thread1, NULL, "toggle_thread1");
+    toggle2 = kthread_create(toggle_thread2, NULL, "toggle_thread2");
+
+    if (toggle2 && toggle1)
+    {
+        wake_up_process(toggle1);
+        wake_up_process(toggle2);
+        printk(KERN_INFO "Created threads\n");
+    }
+    else
+    {
+        printk(KERN_ERR "Problems with threads\n");
+    }
+
     return 0;
 }
 
-static void __exit hello_5_exit(void)
+static void __exit gpio_blink_exit(void)
 {
+    int i;
     printk(KERN_INFO "Goodbye, world 5\n");
+
+    if (toggle1 && toggle2)
+    {
+        kthread_stop(toggle1);
+        kthread_stop(toggle2);
+    }
+
+    // turn all LEDs off
+    for (i = 0; i < arr_argc; i++)
+    {
+        gpio_set_value(gpio_output[i].gpio, 0);
+    }
+
+    // unregister all GPIOs
+    gpio_free_array(gpio_output, arr_argc);
 }
 
-module_init(hello_5_init);
-module_exit(hello_5_exit);
+module_init(gpio_blink_init);
+module_exit(gpio_blink_exit);
